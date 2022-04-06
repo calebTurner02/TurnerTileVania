d1f6eaef9dc869504c06e43cd1ab970479de57be
Caleb Turner <turner632@morrisville.edu>
2022-04-06 16:11:25 -0400

    Sliding on Ladder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float fltRunSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float ClimbSpeed = 5f;
+
+
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
     CapsuleCollider2D myCapsuleCollider;
+    float fltGravityScaleAtStart;
+
  
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        fltGravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     void Update()
@@ -35,7 +40,14 @@ public class PlayerMovement : MonoBehaviour
     void OnJump(InputValue value)
     {
 
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) 
+        {
+           
+            return;
+        }
+
+
+
         if(value.isPressed)
         {
             myRigidBody.velocity += new Vector2 (0f,jumpSpeed);
@@ -65,9 +77,16 @@ public class PlayerMovement : MonoBehaviour
     void ClimbLadder()
     {
 
-         if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {return;}
+         if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+         {
+
+             myRigidBody.gravityScale = fltGravityScaleAtStart; 
+             return;
+         }
+         
 
         Vector2 ClimbVelocity = new Vector2 (myRigidBody.velocity.x, moveInput.y * ClimbSpeed);
         myRigidBody.velocity = ClimbVelocity;
+        myRigidBody.gravityScale = 0;
     }
 }
