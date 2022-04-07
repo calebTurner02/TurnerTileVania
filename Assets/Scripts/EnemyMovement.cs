a2e7822af3ec3924e7936d4ed520177b8b0cc438
Caleb Turner <turner632@morrisville.edu>
2022-04-07 14:47:57 -0400

    Move & Flip Enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float fltMoveSpeed = 1f;
+
+    Rigidbody2D myRigidbody;
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2 (fltMoveSpeed, 0f);
+    }
+
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        fltMoveSpeed = -fltMoveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidbody.velocity.x)), 1f); 
+    }
+}
