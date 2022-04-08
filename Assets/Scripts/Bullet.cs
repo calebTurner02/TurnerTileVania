b92b502026c0cca5acc5ff10d037bbcd37bf21b5
Caleb Turner <turner632@morrisville.edu>
2022-04-08 12:41:40 -0400

    Implement Bullet Behavior

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float fltBulletSpeed = 20f;
+    Rigidbody2D myRigidbody;
+    PlayerMovement player;
+    float fltXSpeed;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        fltXSpeed = player.transform.localScale.x * fltBulletSpeed;
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2 (fltXSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);
+    }
+}
